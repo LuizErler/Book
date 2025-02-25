7513a979fecef85d85ac61e001d68eac0f0968c6
Luiz Erler <luizerler_360@hotmail.com>
2025-02-25 13:30:53 -0300

    ajuste

---
+++ book-front/src/app/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false, // Defina como 'false' para ambiente de desenvolvimento
+    apiBaseUrl: 'http://localhost:3000/api' // Substitua pela URL base da sua API
+  };
+  
---
+++ book-front/src/app/models/livro.model.ts
@@ -0,0 +1,9 @@
+export interface Livro {
+    codl: number;
+    titulo: string;
+    editora?: string;
+    edicao?: number;
+    anoPublicacao?: string;
+    valor: number;
+  }
+  
